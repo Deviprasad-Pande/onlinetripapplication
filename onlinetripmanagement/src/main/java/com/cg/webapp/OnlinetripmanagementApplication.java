1d10e240279f539ed4ffcdd908dd0388c20edac7
samee <samee@DESKTOP-KPNJ6H3.bbrouter>
2022-01-11 07:30:17 +0530

    Log message added

--- onlinetripmanagement/src/main/java/com/cg/webapp/OnlinetripmanagementApplication.java
+++ onlinetripmanagement/src/main/java/com/cg/webapp/OnlinetripmanagementApplication.java
@@ -3,12 +3,16 @@ package com.cg.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class OnlinetripmanagementApplication {
 
 	public static void main(String[] args) {
+	    log.info("Application Started.....");
 		SpringApplication.run(OnlinetripmanagementApplication.class, args);
-	
+	    log.info("Application Configured.....");
 	}
 
 }
