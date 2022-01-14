404c7f6fdc62223c22d6ee52df0090b5a5785479
user <user@Dell.bbrouter>
2022-01-14 23:28:01 +0530

    error deleted

--- onlinetripmanagement/src/main/java/com/cg/webapp/beans/Merchant.java
+++ onlinetripmanagement/src/main/java/com/cg/webapp/beans/Merchant.java
@@ -9,8 +9,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import com.sun.istack.NotNull;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 
+
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
